c611f8fba6031dab4395ed9ed7b1a17421f00040
Mr-balaji <balajidevtar@devtechbrand.com>
2024-09-30 18:12:35 +0530

    setup

---
+++ api/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const mongoose = require('mongoose');  
+require("../config/dbconnect")
+ 
+app.use(express.json()); 
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', require('../routes/useroute'));
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });   
