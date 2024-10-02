f2371ce260357c4b2d4a3ed7afcaefc9d37ea28b
Mr-balaji <balajidevtar@devtechbrand.com>
2024-10-02 16:46:49 +0530

    Revert "some chnages"
    This reverts commit f1bafb5fe66592b66f8aa365f62a68aa52a805d9.

--- controllers/businessownercontroller.js
+++ controllers/businessownercontroller.js
@@ -104,7 +104,7 @@ const loginBusinessOwner = asyncHandler(async (req, res) => {
 
         // Return user data along with the token
         res.status(200).json({
-            data: user.toUserResponse(),
+            user: user.toUserResponse(),
         });
     } catch (error) {
         console.error(error);
