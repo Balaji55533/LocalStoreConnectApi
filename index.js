1d2cfb34f7ef64ba96e931cbcf854235fe34ad9a
Mr-balaji <balajidevtar@devtechbrand.com>
2024-10-03 10:42:47 +0530

    some chnages

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const mongoose = require('mongoose');  
-require("../config/dbconnect")
+require("./config/dbconnect")
  
 app.use(express.json()); 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', require('../routes/useroute'));
-app.use('/api', require('../routes/businessownerroute'));
+app.use('/api', require('./routes/useroute'));
+app.use('/api', require('./routes/businessownerroute'));
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });   
